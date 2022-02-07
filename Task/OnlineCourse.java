d65014802d7c3112644f53470c32a4c9955075d8
sulty02 <98757712+sulty02@users.noreply.github.com>
2022-02-07 17:35:04 +0100

    Create OnlineCourse.java

---
+++ Task/OnlineCourse.java
@@ -0,0 +1,48 @@
+
+package Task_english;
+
+/**
+ *
+ * @author mohamed
+ */
+public class BusinessCourse extends OnlineCourse{
+    
+    public BusinessCourse(){
+        name = id = teacher = "";
+        duration = 0;
+    }
+    
+    public BusinessCourse(String name, String id, String teacher, int duration){
+        this.name = name ="";
+        this.id = id ="";
+        this.teacher = teacher ="";
+        
+        if(checkDurationB(duration)){
+            this.duration = duration;
+        }
+    }
+    
+    public boolean checkDurationB(int duration){
+        if(duration>30){
+            return true;
+        }
+        return false;
+    }
+    
+    public BusinessCourse clone(){
+        BusinessCourse clon;
+        clon = new BusinessCourse();
+        return clon;
+    }
+    
+    public boolean equals(BusinessCourse other){
+        if(this.duration==other.duration && this.id==other.id && this.name==other.name && this.teacher== other.teacher){
+            return true;
+        }
+        return false;
+    }
+    
+    public String toString(){
+        return "Name of the course: " + this.name + ", Teacher: " + this.teacher + ", duration: " + this.duration + ", ID: " + this.id;
+    }
+}
