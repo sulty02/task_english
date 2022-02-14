2e01c65d07c05345fb149d4f6d0087991c180d0e
sulty02 <98757712+sulty02@users.noreply.github.com>
2022-02-14 19:30:42 +0100

    Update Person.java

--- Task/Person.java
+++ Task/Person.java
@@ -3,48 +3,99 @@ package Task_english;
 
 /**
  *
- * @author mohamed
+ * @author ismael
  */
-public class Student extends Person{
-    BusinessCourse course;
-    
-    public Student(){
-        setName("");
-        setSurname("");
-        setEmail("");
-        course = new BusinessCourse();
+public class Person {
+
+    private String name, surname, email;
+
+    public Person() {
+        name = "";
+        surname = "";
+        email = "";
     }
-    
-    public Student(String name, String surname, String email, String nameC, String id, String teacher, String duration){
-        setName(name);
-        setSurname(surname);
-        setEmail(email);
-        course = new BusinessCourse(nameC, id, teacher, duration);
+
+    public Person(String name, String surname, String email) {
+        if (this.compPersonAndSurname()) {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        if (this.compEmail()) {
+            this.email = email;
+        }
     }
-    
-    public Student(Student other){
-        setName(other.getName());
-        setSurname(other.getSurname());
-        setEmail(other.getEmail());
+
+    public Person(Person copy) {
+        name = copy.name;
+        surname = copy.surname;
+        email = copy.email;
     }
-    
-    public boolean equals(Person copy){
-        if(getName()==copy.getName()){
-            if(getSurname()==copy.getSurname()){
-                if(getEmail()==copy.getEmail()){
+
+    public boolean compPersonAndSurname() {
+        if (this.name.length() > 3) {
+            this.surname = this.surname.trim();
+            this.surname = this.surname.toUpperCase();
+            return true;
+        } else {
+            System.out.println("The name is fake.");
+            return false;
+        }
+    }
+
+    public boolean compEmail() {
+        if (this.email.contains("@")) {
+            if (this.email.contains(".es") || this.email.contains(".com")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String toString() {
+        return this.name + " " + this.surname + " with email: " + this.email;
+    }
+
+    public boolean equals(Person copy) {
+        if (this.name == copy.name) {
+            if (this.surname == copy.surname) {
+                if (this.email == copy.email) {
                     return true;
                 }
             }
         }
         return false;
     }
-    
-    public Student clone(Student clon){
-        clon = new Student(this);
-        return clon;
+
+    public Person clone(Person copy) {
+        name = copy.name;
+        surname = copy.surname;
+        email = copy.email;
+        return copy;
     }
     
-    public String toString() {
-        return this.getName() + " " + this.getSurname() + " with email: " + this.getEmail();
-    }
 }
